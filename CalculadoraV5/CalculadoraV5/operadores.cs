391a1dc09d4653ff004cb245eed8edbddc3e2c85
Nicolas Herbert Mietzsch <96891308+Tenshio@users.noreply.github.com>
2022-03-22 09:50:10 -0300

    Add files via upload

---
+++ CalculadoraV5/CalculadoraV5/operadores.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraV5
+{
+    internal class operadores : Form1
+    {
+        public void calculo()
+        {
+
+            switch (operador)
+            {
+                case "+":
+                    total = ultNum + total;
+                    break;
+                case "-":
+                    total = total - ultNum;
+                    break;
+                case "*":
+                    total = ultNum * total;
+                    break;
+                case "/":
+                    total = total / ultNum;
+                    break;
+            }
+        }
+    }
+}
